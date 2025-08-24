7337ea3e095af1faf989d8dff1583a4be8ad370f
Cosimo Chellini <cosimo.chellini@gmail.com>
2025-08-24 13:07:51 +0200

    added other styles

--- data/enum/imageStyle.ts
+++ data/enum/imageStyle.ts
@@ -2,12 +2,22 @@ import { randomItem } from '../../utils/random'
 import { typedObjectValues } from '../../utils/typed'
 
 export enum ImageStyle {
-  VIDEO_GAME = 'video game retro style',
-  CARTOON = 'cartoon style',
-  CARTOON_GHOST_BUSTER = 'cartoon ghostbuster style',
-  GIBHILI = 'gibhili style',
-  ANIME_KAWAII = 'anime kawaii style',
-  TIM_BURTON = 'tim burton style',
+  ANIME = 'anime',
+  ANIME_CHIBI = 'anime chibi',
+  ANIME_KAWAII = 'anime kawaii',
+  CARTOON = 'cartoon',
+  CARTOON_GHOST_BUSTER = 'cartoon ghostbuster',
+  CUBISM = 'cubism',
+  EGYPTIAN_MURAL_PAINTING = 'egyptian mural painting',
+  ETEGAMI_PAINTING = 'etegami painting',
+  GIBHILI = 'gibhili',
+  GOUACHE_PAINTING = 'gouache painting',
+  INK_WASH_PAINTING = 'ink wash painting',
+  LOFI_ART = 'lofi art',
+  NAIVE_ART = 'naive art',
+  TIM_BURTON = 'tim burton',
+  UKYOE_ART = 'ukyo-e art',
+  VIDEO_GAME = 'video game retro',
 }
 
 export const randomImageStyle = () => randomItem(typedObjectValues(ImageStyle))
